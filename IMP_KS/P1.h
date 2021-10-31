5ca1a68cdcbb01055efee86c280691a759b77e29
sdb <2930481693@qq.com>
2021-10-31 16:56:22 +0800

    允许任意语句嵌套

--- IMP_KS/P1.h
+++ IMP_KS/P1.h
@@ -3,10 +3,30 @@
 #include <cassert>
 #include"basic_method.h"
 #include"basicStruct.h"
+#include<stack>
 using namespace std;
 class P1
 {
 public:
+	vector<int> finds(string &now, string aim)
+	{
+		vector<int> res;
+		string rep = "";
+		for (int i = 0; i < aim.length(); i++)
+		{
+			rep += ".";
+		}
+		for (int i = 0; i < now.length(); i++)
+		{
+			int temp = now.find(aim, i);
+			if (temp != -1)
+			{
+				now.replace(temp, aim.length(), rep);
+				res.push_back(temp);
+			}
+		}
+		return res;
+	}
 	vector<string> parseCoProcesses(const string &text) {
 		vector<string> processes;
 		vector<string> processTags;  //代码段标签
@@ -33,25 +53,22 @@ public:
 			std::smatch m;
 			if (std::regex_search(text, m, re)) {
 				string split = string(m[1]);
-				//split = split.left(split.lastIndexOf(';') + 1);//
 				split = split.substr(0, lastIndexOf(split, ';') + 1);//
 
 				split = trimmed(split);
-				//processes << split;
 				processes.push_back(split);
-				//return processes;
 			}
 		}
 
 		return processes;
 	}
 
-	bool parseStatements(const string& input, Statements& statements);
+	bool parseStatements(const string input, Statements& statements);
 
 
 
 	//解析 wait 语句
-	Statement parseWait(const string& input) {
+	Statement parseWait(const string input) {
 		Statement sm;
 		string condition;
 		std::regex re("wait\\((.+)\\)");
@@ -68,7 +85,7 @@ public:
 
 
 	//解析顺序语句
-	Statements parseSequence(const string &input) {
+	Statements parseSequence(const string input) {
 		Statements sms;
 		string inputTrimmed = trimmed(input);
 		//vector<string> list = inputTrimmed.split(';', Qt::SkipEmptyParts);
@@ -88,27 +105,94 @@ public:
 		}
 		return sms;
 	}
+	int modif(string nnw)
+	{
+		 vector<int> r=finds(nnw,"endif");
+		 vector<int> l = finds(nnw, "if");
+		 stack<int> s;
+		 int u = 0, v = 0;
+		 do
+		 {
+			 if (u < l.size())//如果数值
+			 {
+				 if (l[u] < r[v])
+				 {
+					 s.push(l[u]);
+					 u++;
+				 }
+				 else
+				 {
+					 int templ = s.top();
+					 s.pop();
+					 int tempr = r[v];
+					 string uv = "";
+					 for (int i = 0; i < tempr - templ; i++)
+					 {
+						 uv += ".";
+					 }
+					 nnw.replace(templ,tempr-templ,uv);
+					 v++;
+				 }
+			 }
+			 else
+			 {
+				 if (s.empty())
+					 break;
+				 int templ = s.top();
+				 s.pop();
+				 int tempr = r[v];
+				 string uv = "";
+				 for (int i = 0; i < tempr - templ; i++)
+				 {
+					 uv += ".";
+				 }
+				 nnw.replace(templ, tempr - templ, uv);
+				 v++;
+			 }
 
+		 } while (!s.empty());
+		 return nnw.find("else");
+	}
 
 	//解析 if 语句
-	Statement parseIf(const string& input) {
+	Statement parseIf(const string input) {
 		Statement sm;
 		string inputNew = input;
 		//inputNew.remove('\n');
 		inputNew = remove(inputNew, "\n");
 		string condition, ifBody, elseBody;
-
-		if (input.find("else") != -1) {
-			// QRegularExpression re("if(.+)then(.+)else(.+)endif;", QRegularExpression::MultilineOption);
-			std::regex re("if(.+)then(.+)else(.+)endif;"); // TODO: multiline
+		/*
+		if (input.find("else") != -1) {///////////////////不对啊？？？？？？？？？？？？？？？？？？？
+			std::regex re("if(.+?)then(.+)else(.+)endif;"); // TODO: multiline
 			std::smatch m;
 			assert(std::regex_search(inputNew, m, re));
 			condition = trimmed(string(m[1]));
 			ifBody = trimmed(string(m[2]));
 			elseBody = trimmed(string(m[3]));
 		}
+		*/
+		if (input.find("else") != -1) {///////////////////不对啊？？？？？？？？？？？？？？？？？？？
+			std::regex re("if(.+?)then(.+)endif;"); // TODO: multiline
+			std::smatch m;
+			assert(std::regex_search(inputNew, m, re));
+			condition = trimmed(string(m[1]));
+			/////将m[2]中找else
+			string nnw = trimmed(string(m[2]));//输出所有的结果
+			int els=modif(nnw);
+			if (els == -1)
+			{
+				ifBody = nnw;
+			}
+			else
+			{
+				string t1 = nnw.substr(0, els);
+				string t2 = nnw.substr(els + 4, nnw.length() - (els + 4));
+				ifBody = trimmed(t1);
+				elseBody = trimmed(t2);
+			}
+		}
 		else {
-			std::regex re("if(.+)then(.+)endif"); // TODO: multiline
+			std::regex re("if(.+?)then(.+)endif"); // TODO: multiline
 			std::smatch m;
 			assert(std::regex_search(inputNew, m, re));
 			condition = trimmed(string(m[1]));
@@ -124,14 +208,14 @@ public:
 	}
 
 	//解析while语句
-	Statement parseWhile(const string& input) {
+	Statement parseWhile(const string input) {
 		string inputNew = input;
 		//inputNew.remove('\n');
 		inputNew = remove(inputNew, "\n");
 		Statement sm;
 		string condition, body;
 
-		std::regex re("while(.+)do(.+)end");
+		std::regex re("while(.+?)do(.+)end");
 		std::smatch m;
 		if (std::regex_search(inputNew, m, re)) {
 			condition = trimmed(string(m[1]));
@@ -148,52 +232,92 @@ public:
 
 };
 
-bool P1::parseStatements(const string& input, Statements& statements) {
-	int s = 0;
-	int e = 0;
-	while (s < input.length())
+
+bool P1::parseStatements(string input, Statements& statements) {
+	if (input == "")
+		return true;
+	int pos1 = input.find("if");
+	int pos2 = input.find("while");
+	int pos;
+	if (pos1 == -1)
 	{
-		int pos1 = input.find("if", s);
-		int pos2 = input.find("while", s);
-		int pos;
-		if (pos1 == -1)
+		pos = pos2;
+	}
+	else if (pos2 == -1)
+	{
+		pos = pos1;
+	}
+	else
+	{
+		pos = min(pos1, pos2);
+	}
+	if (pos1 == -1 && pos2 == -1)//都没有找到while和endwhile
+	{
+		auto states_tmp = parseSequence(input);
+		statements.insert(statements.end(), states_tmp.begin(), states_tmp.end());
+	}
+	else
+	{
+		if (pos != 0)
 		{
-			pos = pos2;
+			string rst = input.substr(0, pos);
+			auto states_temps = parseSequence(rst);//前面仍然存在公式
 		}
-		else if(pos2==-1)
+		string bl, el;
+		if (input.at(pos) == 'i')//找到if了
 		{
-			pos = pos1;
+			bl = "if";
+			el = "endif";
 		}
-		else
+		else//找到while了
 		{
-			pos = min(pos1, pos2);
+			bl = "while";
+			el = "endwhile";
 		}
-		if (pos1==-1&&pos2==-1)//都没找到
+		stack<int> aim;
+		string see = input.substr(pos,input.length()-pos);//从头开始匹配
+		vector<int>r = finds(see, el);
+		vector<int>l= finds(see,bl);
+		aim.push(l[0]);
+		int li=1;
+		int ri=0;
+		while (!aim.empty())
 		{
-			e = input.length();
-			string inputSplit = input.substr(s, e - s);
-			auto states_tmp = parseSequence(inputSplit);
-			statements.insert(statements.end(), states_tmp.begin(), states_tmp.end());
-			s = e;
-		}
-		else {
-			if (pos > s) {
-				auto states_tmp = parseSequence(input.substr(s, pos - s));
-				statements.insert(statements.end(), states_tmp.begin(), states_tmp.end());
+			if (li < l.size()&&l[li] < r[ri])
+			{
+				aim.push(l[li]);
+				li++;
 			}
-			s = pos;
-			if (input.at(pos) == 'i') {
-				e = input.find("endif");
-				e += 6;
-				statements.push_back(parseIf(input.substr(s, e - s)));
+			else
+			{
+				aim.pop();
+				if (aim.empty())
+				{
+					string sev= input.substr(pos, input.length() - pos);
+					if (bl.at(0) == 'i')
+					{
+						string u1 = sev.substr(0, r[ri] + 6);
+						string u2 = sev.substr(r[ri] + 6, sev.length() - (r[ri] + 6));
+						statements.push_back(parseIf(u1));
+						parseStatements(u2, statements);
+					}
+					else
+					{
+						string u1 = sev.substr(0, r[ri] + 9);
+						string u2 = sev.substr(r[ri] + 9, sev.length() - (r[ri] + 9));
+						statements.push_back(parseWhile(u1));
+						parseStatements(u2, statements);
+					}
+					//找到了
+					return true;
+				}
+				else
+				{
+					ri++;
+				}
 			}
-			else {
-				e = input.find("endwhile");
-				e += 9;
-				statements.push_back(parseWhile(input.substr(s, e - s)));
-			}
-			s = e;
 		}
+
 	}
 	return true;
 }
